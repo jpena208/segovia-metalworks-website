db9f9581a8fd9bac3be5dcb9d67b2387eb34fe0a
jpena208 <49579138+jpena208@users.noreply.github.com>
2022-06-25 17:53:55 -0500

    Added 5 items to shop

--- js/index.js
+++ js/index.js
@@ -56,6 +56,45 @@ function changeText(product, number) {
   let firePitPrice2 = "$450";
   let firePitPrice3 = "$550";
 
+  // Text for Plain Lids
+  let plainLidTitle1 = '16" Plain Lid';
+  let plainLidTitle2 = '20" Plain Lid';
+  let plainLidTitle3 = '24" Plain Lid';
+
+  let plainLidText1 = '16" and 3/16" thick.';
+  let plainLidText2 = '20" and 3/16" thick.';
+  let plainLidText3 = '24" and 3/16" thick.';
+
+  let plainLidPrice1 = "$30";
+  let plainLidPrice2 = "$40";
+  let plainLidPrice3 = "$50";
+
+  // Text for Lids with damper
+  let LidWDTitle1 = '16" Lid With Damper';
+  let LidWDTitle2 = '20" Lid With Damper';
+  let LidWDTitle3 = '24" Lid With Damper';
+
+  let LidWDText1 = '16" and 3/16" thick.';
+  let LidWDText2 = '20" and 3/16" thick.';
+  let LidWDText3 = '24" and 3/16" thick.';
+
+  let LidWDPrice1 = "$35";
+  let LidWDPrice2 = "$45";
+  let LidWDPrice3 = "$55";
+
+  // Text for Lids with damper
+  let damperTitle1 = '16" Damper';
+  let damperTitle2 = '20" Damper';
+  let damperTitle3 = '24" Damper';
+
+  let damperText1 = '16" and 1/8" thick.';
+  let damperText2 = '20" and 1/8" thick.';
+  let damperText3 = '24" and 1/8" thick.';
+
+  let damperPrice1 = "$10";
+  let damperPrice2 = "$12.50";
+  let damperPrice3 = "$15";
+
   if (product == 1) {
     if (number == 1) {
       document.getElementById("grillTitle").innerHTML = grillTitle1;
@@ -105,6 +144,57 @@ function changeText(product, number) {
       document.getElementById("firepitTitle").innerHTML = firePitTitle3;
       document.getElementById("firepitText").innerHTML = firePitText3;
       document.getElementById("firepitPrice").innerHTML = firePitPrice3;
+    }
+  }
+  if (product == 4) {
+    if (number == 1) {
+      document.getElementById("plainLidTitle").innerHTML = plainLidTitle1;
+      document.getElementById("plainLidText").innerHTML = plainLidText1;
+      document.getElementById("plainLidPrice").innerHTML = plainLidPrice1;
+    }
+    if (number == 2) {
+      document.getElementById("plainLidTitle").innerHTML = plainLidTitle2;
+      document.getElementById("plainLidText").innerHTML = plainLidText2;
+      document.getElementById("plainLidPrice").innerHTML = plainLidPrice2;
+    }
+    if (number == 3) {
+      document.getElementById("plainLidTitle").innerHTML = plainLidTitle3;
+      document.getElementById("plainLidText").innerHTML = plainLidText3;
+      document.getElementById("plainLidPrice").innerHTML = plainLidPrice3;
+    }
+  }
+  if (product == 5) {
+    if (number == 1) {
+      document.getElementById("lidWDTitle").innerHTML = LidWDTitle1;
+      document.getElementById("lidWDText").innerHTML = LidWDText1;
+      document.getElementById("lidWDPrice").innerHTML = LidWDPrice1;
+    }
+    if (number == 2) {
+      document.getElementById("lidWDTitle").innerHTML = LidWDTitle2;
+      document.getElementById("lidWDText").innerHTML = LidWDText2;
+      document.getElementById("lidWDPrice").innerHTML = LidWDPrice2;
+    }
+    if (number == 3) {
+      document.getElementById("lidWDTitle").innerHTML = LidWDTitle3;
+      document.getElementById("lidWDText").innerHTML = LidWDText3;
+      document.getElementById("lidWDPrice").innerHTML = LidWDPrice3;
+    }
+  }
+  if (product == 6) {
+    if (number == 1) {
+      document.getElementById("damperTitle").innerHTML = damperTitle1;
+      document.getElementById("damperText").innerHTML = damperText1;
+      document.getElementById("damperPrice").innerHTML = damperPrice1;
+    }
+    if (number == 2) {
+      document.getElementById("damperTitle").innerHTML = damperTitle2;
+      document.getElementById("damperText").innerHTML = damperText2;
+      document.getElementById("damperPrice").innerHTML = damperPrice2;
+    }
+    if (number == 3) {
+      document.getElementById("damperTitle").innerHTML = damperTitle3;
+      document.getElementById("damperText").innerHTML = damperText3;
+      document.getElementById("damperPrice").innerHTML = damperPrice3;
     }
   }
 }
